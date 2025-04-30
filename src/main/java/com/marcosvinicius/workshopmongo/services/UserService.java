51b07f8cfd39711fa06d546ade08c545f167f14c
Marcos Vinicius <marcos.carini22@gmail.com>
2025-04-30 03:34:55 -0300

    add findById method

--- src/main/java/com/marcosvinicius/workshopmongo/services/UserService.java
+++ src/main/java/com/marcosvinicius/workshopmongo/services/UserService.java
@@ -2,6 +2,7 @@ package com.marcosvinicius.workshopmongo.services;
 
 import com.marcosvinicius.workshopmongo.domain.User;
 import com.marcosvinicius.workshopmongo.repository.UserRepository;
+import com.marcosvinicius.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,4 +16,10 @@ public class UserService {
     public List<User> findAll(){
         return  repo.findAll();
     }
+
+    public User findById(String id) {
+        return repo.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
 }
