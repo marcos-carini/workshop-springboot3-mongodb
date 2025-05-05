d8216bd1122e04a30090a4a35be074c0080790b9
Marcos Vinicius <marcos.carini22@gmail.com>
2025-05-05 18:28:22 -0300

    feat: add Post service and resource

---
+++ src/main/java/com/marcosvinicius/workshopmongo/services/PostService.java
@@ -0,0 +1,21 @@
+package com.marcosvinicius.workshopmongo.services;
+
+import com.marcosvinicius.workshopmongo.domain.Post;
+
+import com.marcosvinicius.workshopmongo.repository.PostRepository;
+import com.marcosvinicius.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PostService {
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        return repo.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
