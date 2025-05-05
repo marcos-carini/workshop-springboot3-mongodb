b905af53f42e9873892c6eed5b5ce528479633fc
Marcos Vinicius <marcos.carini22@gmail.com>
2025-05-05 19:32:19 -0300

    feat: add simple query with spring query methods

--- src/main/java/com/marcosvinicius/workshopmongo/resources/PostResource.java
+++ src/main/java/com/marcosvinicius/workshopmongo/resources/PostResource.java
@@ -1,10 +1,13 @@
 package com.marcosvinicius.workshopmongo.resources;
 
 import com.marcosvinicius.workshopmongo.domain.Post;
+import com.marcosvinicius.workshopmongo.resources.util.URL;
 import com.marcosvinicius.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -17,6 +20,14 @@ public class PostResource {
         Post obj = service.findById(id);
 
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(value="/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value ="text", defaultValue = "") String text){
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+
+        return ResponseEntity.ok().body(list);
     }
 
 }
