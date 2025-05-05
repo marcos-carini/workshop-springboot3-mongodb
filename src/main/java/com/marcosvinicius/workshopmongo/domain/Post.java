42f189113e953ed514dd2879a983122d22bd4d04
Marcos Vinicius <marcos.carini22@gmail.com>
2025-05-05 19:06:21 -0300

    feat: add comments for posts

--- src/main/java/com/marcosvinicius/workshopmongo/domain/Post.java
+++ src/main/java/com/marcosvinicius/workshopmongo/domain/Post.java
@@ -1,11 +1,14 @@
 package com.marcosvinicius.workshopmongo.domain;
 
 import com.marcosvinicius.workshopmongo.dto.AuthorDTO;
+import com.marcosvinicius.workshopmongo.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -18,6 +21,7 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post(){
 
@@ -71,6 +75,14 @@ public class Post implements Serializable {
         this.author = author;
     }
 
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -83,6 +95,7 @@ public class Post implements Serializable {
     public int hashCode() {
         return Objects.hash(id);
     }
+
 
 
 }
